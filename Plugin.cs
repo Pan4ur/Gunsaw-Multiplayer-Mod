cb993994b703bd13fa3b644013d23e8b967131fe
Pan4ur <lifanov2005@mail.ru>
2026-07-30 00:04:42 +0200

    0.4.1

--- Plugin.cs
+++ Plugin.cs
@@ -17,7 +17,7 @@ public sealed class GunsawMultiplayerPlugin : BaseUnityPlugin
 {
     public const string PluginGuid = "com.gunsaw.multiplayer";
     public const string PluginName = "Gunsaw Multiplayer";
-    public const string PluginVersion = "0.4.0";
+    public const string PluginVersion = "0.4.1";
     private const string ReleasesApiUrl = "https://api.github.com/repos/Pan4ur/Gunsaw-Multiplayer-Mod/releases/latest";
     internal static GunsawMultiplayerPlugin Instance { get; private set; }
 
