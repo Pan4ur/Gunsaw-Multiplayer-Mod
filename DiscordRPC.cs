a59ddb0b0ca5f9f3406917677bd55102feaf738e
rushell <132660417+rushellxyz@users.noreply.github.com>
2026-08-17 13:44:41 +0300

    Option to disable

--- DiscordRPC.cs
+++ DiscordRPC.cs
@@ -1,17 +1,56 @@
+using UnityEngine.UI;
 using UnityEngine;
+using HarmonyLib;
+using TMPro;
 using DiscordRPC;
+
+// Creates RPC toggle button in setting
+[HarmonyPatch(typeof(ControlBinder), "Start")]
+internal static class RPCSettings
+{
+    private static void Postfix(ControlBinder __instance)
+    {
+        RPCManager.CheckInstance();
+        GameObject crossToggle = GameObject.Find("Canvas/Settings/CrosshairSettings/CrossToggle");
+        GameObject rpcToggle = UnityEngine.Object.Instantiate(crossToggle);
+        rpcToggle.transform.SetParent(crossToggle.transform.parent);
+        rpcToggle.transform.localScale = crossToggle.transform.localScale;
+        rpcToggle.transform.localPosition = new Vector3(crossToggle.transform.localPosition.x, crossToggle.transform.localPosition.y - 125f, crossToggle.transform.localPosition.z);
+        Toggle rpcToggleToggle = rpcToggle.GetComponent<Toggle>();
+        rpcToggleToggle.isOn = 0 == PlayerPrefs.GetInt("rpcdisable");
+        rpcToggleToggle.onValueChanged = new Toggle.ToggleEvent();
+        rpcToggleToggle.onValueChanged.AddListener(ToggleRPC);
+
+        GameObject mainName = GameObject.Find("Canvas/Settings/CrosshairSettings/MainName (13)");
+        GameObject rpcName = UnityEngine.Object.Instantiate(mainName);
+        rpcName.transform.SetParent(mainName.transform.parent);
+        rpcName.transform.localScale = mainName.transform.localScale;
+        rpcName.transform.localPosition = new Vector3(mainName.transform.localPosition.x, mainName.transform.localPosition.y - 125f, mainName.transform.localPosition.z);
+        rpcName.GetComponent<TextMeshProUGUI>().text = "Discord RPC";
+    }
+
+    private static void ToggleRPC(bool isOn)
+    {
+        UnityEngine.Debug.Log(isOn);
+        RPCManager.instance.enable = isOn;
+        if (isOn)
+            PlayerPrefs.SetInt("rpcdisable", 0);
+   else     PlayerPrefs.SetInt("rpcdisable", 1);
+    }
+}
 
 internal sealed class RPCManager : MonoBehaviour
 {   // Stole that code from CU, and dll
-    // Would need TODO cleanup TODO option to disable TODO actual rpc
-    private static RPCManager instance;
+    // Would need TODO cleanup TODO actual rpc
+    public bool enable;
+    public static RPCManager instance;
     private DiscordRpcClient client;
     private float timer = 5f;
 
     public static void CheckInstance()
     {
         if (!instance)
-        {
+        { // Inverted, so its on by default
             new GameObject("RPCManager", typeof(RPCManager));
         }
     }
@@ -19,6 +58,7 @@ internal sealed class RPCManager : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        enable = 0 == PlayerPrefs.GetInt("rpcdisable");
         UnityEngine.Object.DontDestroyOnLoad(base.gameObject);
     }
 
@@ -29,12 +69,11 @@ internal sealed class RPCManager : MonoBehaviour
         {
             timer = 5f;
             client?.SetPresence(GetCurrentRichPresence());
-            bool value = true;//Settings.Get<SettingBool>("rpc").value;
-            if (value && client == null)
+            if (enable && client == null)
             {
                 Initialize();
             }
-            if (!value && client != null)
+            if (!enable && client != null)
             {
                 DestroyClient();
             }
