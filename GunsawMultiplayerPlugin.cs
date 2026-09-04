742a4b801185dbb8279e8959b842c0f2415eb530
Pan4ur <lifanov2005@mail.ru>
2026-09-04 15:00:39 +0300

    0.4.7

--- GunsawMultiplayerPlugin.cs
+++ GunsawMultiplayerPlugin.cs
@@ -14,7 +14,7 @@ public sealed class GunsawMultiplayerPlugin : BaseUnityPlugin
 {
     public const string PluginGuid = "com.gunsaw.multiplayer";
     public const string PluginName = "Gunsaw Multiplayer";
-    public const string PluginVersion = "0.4.6";
+    public const string PluginVersion = "0.4.7";
     private const string ReleasesApiUrl = "https://api.github.com/repos/Pan4ur/Gunsaw-Multiplayer-Mod/releases/latest";
     private const string CustomLevelsUrl = "https://github.com/jimmyking9999999/gunsaw-level-editor-plus/raw/refs/heads/main/Levels.json";
     private const string ServersUrl = "https://raw.githubusercontent.com/Pan4ur/Gunsaw-Multiplayer-Mod/main/Assets/servers.json";
